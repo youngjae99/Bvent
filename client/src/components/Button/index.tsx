f12c0a842ed77fc312937e1e93b4eb051bc97f9d
Youngjae Jang <youngjae.j99@gmail.com>
2022-09-07 00:52:06 +0900

    feat: button disabled

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -8,8 +8,18 @@ export type IButton = React.DetailedHTMLProps<
 const Button: React.FC<IButton> = ({
   className = '',
   children,
+  disabled,
   ...rest
 }: any) => {
+  if(disabled){
+    return (<button
+      disabled={true}
+      className={`disabled py-2 px-4 rounded-lg bg-gray-400 font-bold text-outline-none ring-opacity-75 text-white text-lg cursor-default`}
+      {...rest}
+    >
+      {children}
+    </button>);
+  }
   return (
     <button
       className={`py-2 px-4 rounded-lg bg-blue-400 font-bold text- hover:bg-blue-500 outline-none ring-opacity-75 ring-green-400 focus:ring text-white text-lg ${className} transition-all`}
